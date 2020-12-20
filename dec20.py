0df45f35141fc8bc437998da38a7be6b957177f6
Jeff Sumner <jeff.sumner@gmail.com>
2020-12-20 08:17:20 -0600

    Refactored to use Tile class

--- dec20.py
+++ dec20.py
@@ -21,7 +21,25 @@ class Tile:
     def add_gridline(self, line):
         self.grid.append(line)
 
-tiles = {}
+    def get_grid(self):
+        return self.grid
+
+    def get_side(self, side, flipped):
+        ret_val = ''
+        if side == 0:
+            ret_val = self.grid[0]
+        elif side == 2:
+            ret_val = self.grid[-1]
+        else:
+            for g in self.grid:
+                if side == 1:
+                    ret_val += g[0]
+                elif side == 3:
+                    ret_val += g[-1]
+        if flipped:
+            return ret_val[::-1]
+        return ret_val
+
 ntiles = {}
 
 def print_tiles():
@@ -48,92 +66,48 @@ def dec20_1():
         for cnt, line in enumerate(f):
             data.append(line.strip())
 
+    # Make tiles
     regex = re.compile('Tile (\d+):')
     tile = 0
     for d in data:
         match = regex.match(d)
         if match:
             tile = match.group(1)
-            tiles[tile] = {'tile': [], 'matches': []}
             ntiles[tile] = Tile()
         elif d == "":
             continue
         else:
-            tiles[tile]['tile'].append(d)
             ntiles[tile].add_gridline(d)
     # print_tiles()
-    print(tiles)
 
+    # Makes sides
     sides = {}
-    for k, t in tiles.items():
-        loc = {'tile': k, 'side': 1, 'flip': False}
-        side = t['tile'][0]
-        sides = add_side(sides, side, loc)
-        loc = {'tile': k, 'side': 1, 'flip': True}
-        side = t['tile'][0][::-1]
-        sides = add_side(sides, side, loc)
-        loc = {'tile': k, 'side': 3, 'flip': False}
-        side = t['tile'][-1]
-        sides = add_side(sides, side, loc)
-        loc = {'tile': k, 'side': 3, 'flip': True}
-        side = t['tile'][-1][::-1]
-        sides = add_side(sides, side, loc)
-        s2f = ''
-        s4f = ''
-        for i in range(len(t['tile'])):
-            s2f += t['tile'][i][-1]
-            s4f += t['tile'][i][0]
-        s2t = s2f[::-1]
-        s4t = s4f[::-1]
-        loc = {'tile': k, 'side': 2, 'flip': False}
-        side = s2f
-        if side not in sides:
-            sides[side] = [loc]
-        else:
-            sides[side].append(loc)
-        loc = {'tile': k, 'side': 2, 'flip': True}
-        side = s2t
-        sides = add_side(sides, side, loc)
-        loc = {'tile': k, 'side': 4, 'flip': False}
-        side = s4f
-        sides = add_side(sides, side, loc)
-        loc = {'tile': k, 'side': 4, 'flip': True}
-        side = s4t
-        sides = add_side(sides, side, loc)
-
-    print(sides)
-
-    count = 0
-    for k, v in sides.items():
-
-        my_tiles = []
-        for t in v:
-            my_tiles.append(t['tile'])
-        print(k, v, my_tiles)
-        if len(v) > 1:
-
-            count += 1
-            print('s', k, len(v), count)
+    for k, t in ntiles.items():
+        for i in range(4):
+            # print(k, i)
+            loc = {'tile': k, 'side': i, 'flip': False}
+            side = t.get_side(i, False)
+            sides = add_side(sides, side, loc)
+            loc = {'tile': k, 'side': i, 'flip': True}
+            side = t.get_side(i, True)
+            sides = add_side(sides, side, loc)
 
     total = 1
-    for k, v in tiles.items():
+    for k, v in ntiles.items():
         count = 0
         # print('Checking', k)
-        side = sides[v['tile'][0]]
+        side = sides[v.get_side(0, False)]
         for s in side:
             if s['tile'] != k:
                 # print('top', s)
                 count += 1
-        side = sides[v['tile'][-1]]
+        side = sides[v.get_side(2, False)]
         for s in side:
             if s['tile'] != k:
                 # print('bot', s)
                 count += 1
-        left = ''
-        right = ''
-        for i in range(len(v['tile'])):
-            left += v['tile'][i][0]
-            right += v['tile'][i][-1]
+        left = v.get_side(3, False)
+        right = v.get_side(1, False)
         side = sides[left]
         for s in side:
             if s['tile'] != k:
@@ -151,18 +125,18 @@ def dec20_1():
 
     big_tile = []
     start = '2801'
-    print(tiles[start])
+    print(ntiles[start])
 
 
 
     pounds = 0
-    for k, v in tiles.items():
-        for l in v['tile']:
+    for k, v in ntiles.items():
+        for l in v.get_grid():
             for c in l[1:-1]:
                 if c == '#':
                     pounds += 1
     while pounds > 2013:
-        if pounds < 2523 and pounds not in(2253, 2133, 2118, 2103):
+        if pounds < 2523 and pounds not in (2253, 2133, 2118, 2103, 2088, 2073, 2058):
             print(pounds)
         pounds -= 15
 
