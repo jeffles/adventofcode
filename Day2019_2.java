9301859e3d3aca2b9bddf5c0bfa72c86390d9a08
Jeff Sumner <jeff.sumner@gmail.com>
2021-02-01 07:49:16 -0600

    Refactor to remove duplicate code

--- Day2019_2.java
+++ Day2019_2.java
@@ -17,9 +17,30 @@ public class Day2019_2 {
         }
         myReader.close();
 
+        int answer = runProgram(startingMemory, 12, 2);
+        System.out.println("Part 1 " + answer);
+        part2(startingMemory);
+
+    }
+    private static int part2(ArrayList<Integer> startingMemory) {
+        for (int noun = 0; noun <= 99; noun++){
+            for (int verb = 0; verb <= 99; verb++){
+                int answer = runProgram(startingMemory, noun, verb);
+
+                if (answer == 19690720) {
+                    System.out.printf("Part 2 %s%s\n", noun, verb);
+                    return 1;
+                }
+
+            }
+        }
+        return 1;
+    }
+
+    private static int runProgram(ArrayList<Integer>startingMemory, int noun, int verb) {
         List<Integer> memory = new ArrayList<>(startingMemory);
-        memory.set(1, 12);
-        memory.set(2, 2);
+        memory.set(1, noun);
+        memory.set(2, verb);
         int index = 0;
         while (memory.get(index) != 99) {
             int instruction = memory.get(index);
@@ -34,38 +55,7 @@ public class Day2019_2 {
             }
             index += 4;
         }
-        System.out.println("Part 1 " + memory.get(0));
-        part2(startingMemory);
-
-    }
-    private static int part2(ArrayList<Integer> startingMemory) {
-        for (int noun = 0; noun <= 99; noun++){
-            for (int verb = 0; verb <= 99; verb++){
-                List<Integer> memory = new ArrayList<>(startingMemory);
-                memory.set(1, noun);
-                memory.set(2, verb);
-                int index = 0;
-                while (memory.get(index) != 99) {
-                    int instruction = memory.get(index);
-                    if (instruction == 1){
-                        memory.set(memory.get(index+3), memory.get(memory.get(index+1)) + memory.get(memory.get(index+2)));
-                    }
-                    else if (instruction == 2){
-                        memory.set(memory.get(index+3), memory.get(memory.get(index+1)) * memory.get(memory.get(index+2)));
-                    }
-                    else {
-                        System.out.println("ERR");
-                    }
-                    index += 4;
-                }
-                if (memory.get(0) == 19690720) {
-                    System.out.printf("Part 2 %s%s\n", noun, verb);
-                    return 1;
-                }
-
-            }
-        }
-        return 1;
+        return (memory.get(0));
     }
 
 }
