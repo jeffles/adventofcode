59abbd1f11f5ad302acc50f9c979ad695bae98c8
Jeff Sumner <jeff.sumner@gmail.com>
2021-12-18 13:00:05 -0600

    Fixed part 2

--- 2021/D18.py
+++ 2021/D18.py
@@ -5,8 +5,12 @@ import sys
 import unittest
 import math
 
-
 def process(d):
+    while sub_process(d):
+        pass
+    return d
+
+def sub_process(d):
     match_depth = 1
     for x in range(len(d)):
         if d[x]['d'] < 5:
@@ -20,7 +24,7 @@ def process(d):
             d[x]['d'] -= 1
             d[x]['v'] = 0
             d.pop(x - 1)
-            # print('EXPLODE')
+            # print('E', dprint(d))
             # dprint(d)
             return True
         else:
@@ -34,7 +38,7 @@ def process(d):
             d[x]['v'] = math.floor(value / 2)
             new = {'d': d[x]['d'], 'v': math.ceil(value / 2)}
             d.insert(x+1, new)
-            # dprint(d)
+            # print('D', dprint(d))
             return True
 
 
@@ -82,30 +86,7 @@ def parse(d):
             depth -= 1
     # print(dstack)
     return dstack
-    #
-    # # print('Looking at', d)
-    # depth = 0
-    # ll = []
-    # if d[0].isdigit():
-    #     ll.append(int(d[0]))
-    #     if len(d) > 2:
-    #         ll.append(parse(d[2:])[0])
-    # end = 0
-    # if d[0] == '[':
-    #     for x in range(len(d)):
-    #         if d[x] == '[':
-    #             depth += 1
-    #         if d[x] == ']':
-    #             depth -= 1
-    #             if depth == 0:
-    #                 end = x
-    #                 break
-    #     ll.append(parse(d[1:end]))
-    #     # print(end, len(d))
-    #     if end+2 < len(d):
-    #         ll.append(parse(d[end+2:len(d)])[0])
-    #
-    # return ll
+
 
 
 def dprint(d):
@@ -126,14 +107,15 @@ def dprint(d):
     line += ']'
     return line
 
-
 def dadd(l, r):
+    new = []
     for d in l:
-        d['d'] += 1
+        new.append(d)
     for d in r:
+        new.append(d)
+    for d in new:
         d['d'] += 1
-        l.append(d)
-    return l
+    return new
 
 def magnitude(d):
     match_depth = 0
@@ -143,10 +125,25 @@ def magnitude(d):
                 d[x]['d'] -= 1
                 d[x]['v'] = 2 * d[x]['v'] + 3 * d[x-1]['v']
                 d.pop(x-1)
+                # print(dprint(d))
                 break
             else:
                 match_depth = d[x]['d']
     return d[0]['v']
+
+def tests():
+
+    assert 143 == magnitude(process(parse('[[1,2],[[3,4],5]]'))), "Problem"
+    assert 1384 == magnitude(process(parse('[[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]]'))), "Problem"
+    assert 445 == magnitude(process(parse('[[[[1, 1], [2, 2]], [3, 3]], [4, 4]]'))), "Problem"
+    assert 791 == magnitude(process(parse('[[[[3, 0], [5, 3]], [4, 4]], [5, 5]]'))), "Problem"
+    assert 1137 == magnitude(process(parse('[[[[5, 0], [7, 4]], [5, 5]], [6, 6]]'))), "Problem"
+    assert 3488 == magnitude(process(parse('[[[[8, 7], [7, 7]], [[8, 6], [7, 7]]], [[[0, 7], [6, 6]], [8, 7]]]'))), "Problem"
+
+    left = process(parse('[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]'))
+    right = process(parse('[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]'))
+    sum = dadd(left, right)
+    assert 3993 == magnitude(process(sum)), "Problem"
 
 def part1():
 
@@ -155,22 +152,12 @@ def part1():
     with open('input', 'r') as f:
         for cnt, line in enumerate(f):
             data.append(line.strip())
-    # data = ['[[[[[9,8],1],2],3],4]']
-    # data.append('[7,[6,[5,[4,[3,2]]]]]')
-    # data.append('[[6,[5,[4,[3,2]]]],1]')
-    # data.append('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]')
-    # data.append('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]')
-    # data.append('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]')
 
     for x in range(len(data)):
         data[x] = parse(data[x])
 
     for d in data:
-        # print('PROCESSING')
-        # dprint(d)
-        while process(d):
-            pass
-            # dprint(d)
+        process(d)
 
     #Add
     print('ADD')
@@ -180,9 +167,8 @@ def part1():
         dprint(r)
         l = dadd(l, r)
         dprint(l)
-        while process(l):
-            pass
-        print('After')
+        process(l)
+
         dprint(l)
 
     #Magnitude
@@ -199,29 +185,23 @@ def part2():
             data.append(line.strip())
 
     for x in range(len(data)):
-        data[x] = parse(data[x])
-
-    for d in data:
-        # print('PROCESSING')
-        # dprint(d)
-        while process(d):
-            pass
-            # dprint(d)
+        data[x] = process(parse(data[x]))
 
     max_magnitude = 0
-    for first in range(len(data)):
-        for second in range(len(data)):
-            if first == second:
-                continue
-            l = data[first][:]
-            r = data[second][:]
+    for x in data:
+        # print(dprint(x))
+        for y in data:
+            if x != y:
 
-            mysum = dadd(l, r)
+                # print(dprint(x),  dprint(y))
+                step1 = dadd(copy.deepcopy(x), copy.deepcopy(y))
+                step2 = process(copy.deepcopy(step1))
+                # print(dprint(step2))
+                score = magnitude((copy.deepcopy(step2)))
 
-            mymag = magnitude(mysum)
-            if mymag > max_magnitude:
-                max_magnitude = mymag
-                print('new max', max_magnitude, first, second)
+                if score > max_magnitude:
+                    max_magnitude = score
+                    print('new max', max_magnitude)
 
 
     print('Part 2', max_magnitude)
@@ -229,4 +209,6 @@ def part2():
 
 if __name__ == '__main__':
     # unittest.main()
+    tests()
+
     part2()
